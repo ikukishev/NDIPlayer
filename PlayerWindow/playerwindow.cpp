d552ec0c0649fe8b9b7c4ca701045c5366110e1b
Ivan Kukishev <IKukishev@luxoft.com>
2021-03-11 23:09:55 +0200

    Initial commit

---
+++ PlayerWindow/playerwindow.cpp
@@ -0,0 +1,11 @@
+#include "playerwindow.h"
+
+PlayerWindow::PlayerWindow(QWidget *parent)
+   : QWidget(parent)
+{
+}
+
+PlayerWindow::~PlayerWindow()
+{
+}
+
---
+++ PlayerWindow/playerwindow.h
@@ -0,0 +1,16 @@
+#ifndef PLAYERWINDOW_H
+#define PLAYERWINDOW_H
+
+#include <QWidget>
+
+class PlayerWindow : public QWidget
+{
+   Q_OBJECT
+
+public:
+   PlayerWindow(QWidget *parent = nullptr);
+   ~PlayerWindow();
+
+private:
+};
+#endif // PLAYERWINDOW_H
